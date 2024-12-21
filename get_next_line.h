4ba4176da14387d560ab072be54b5f81f56df580
Resul A. <39741608+Ustrif@users.noreply.github.com>
2024-12-21 18:19:06 +0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raydogmu <raydogmu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/14 13:13:03 by raydogmu          #+#    #+#             */
+/*   Updated: 2024/12/14 13:41:46 by raydogmu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -14,5 +26,6 @@ char	*ft_strjoin(char *s1, char *s2);
 int		ft_strlen(const char *s);
 char	*line_control(char **line);
 char	*ft_strchr(const char *s, int c);
+void	free_if_empty(char **line);
 
 #endif
