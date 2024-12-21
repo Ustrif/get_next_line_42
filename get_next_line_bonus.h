9148416b2621f587b8ed7158e5aa1b54bc025ed5
Resul A. <39741608+Ustrif@users.noreply.github.com>
2024-12-21 18:20:08 +0300

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raydogmu <raydogmu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/14 13:12:50 by raydogmu          #+#    #+#             */
+/*   Updated: 2024/12/14 13:35:53 by raydogmu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
@@ -14,5 +26,6 @@ char	*ft_strjoin(char *s1, char *s2);
 int		ft_strlen(const char *s);
 char	*line_control(char **line);
 char	*ft_strchr(const char *s, int c);
+void	free_if_empty(char **line);
 
 #endif
